80baf3b28662119d5054979ae37d75ad96460cf2
riskypenguin <maximilian-schalk@web.de>
2023-03-13 00:00:20 +0100

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Ingredient, type Inventory } from '$lib/data';
+
+	const ingredients = Object.values(Ingredient);
+
+	const inventory: Inventory = {
+		Leaf: 0,
+		Sand: 0,
+		Water: 0,
+		Lightning: 0,
+		Poison: 0,
+		Beetle: 0,
+		Tooth: 0,
+		Flame: 0,
+		Steel: 0,
+		Scale: 0,
+		Essence: 0,
+		Power: 0,
+		Shadow: 0,
+		Spirit: 0
+	};
+</script>
+
+<h1>TT2 Alchemy Event Optimizer</h1>
+
+<table>
+	<thead>
+		<caption>Inventory</caption>
+		<tr>
+			<td>Ingredient</td>
+			<td>Count</td>
+		</tr>
+	</thead>
+	<tbody>
+		{#each ingredients as ingredient}
+			<tr>
+				<td>{ingredient}</td>
+				<td>
+					<input type="number" bind:value={inventory[ingredient]} />
+				</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+</style>
